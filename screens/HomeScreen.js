77241192c56e305b97b17c87fa88c0b6469bc040
Arush Kumar <52474182+02Arush@users.noreply.github.com>
2023-04-08 15:45:27 -0400

    Edited Homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,74 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, SafeAreaView, View } from "react-native";
+import React from "react";
+import Constants from 'expo-constants';
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
-  )
-}
+    <View style={styles.container}>
+      <Header />
+      <Body />
+      <NavBar />
+    </View>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const Header = () => {
+  return (
+    <View style={styles.Header}>
+      <Text>Events</Text>
+    </View>
+  );
+};
+
+const Body = () => {
+  return (
+    <View style={styles.Body}>
+      <Text>Body</Text>
+    </View>
+  );
+};
+
+const NavBar = () => {
+  return (
+    <View style={styles.NavBar}>
+      <Text>Navbar</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "top",
+  },
+
+  Header: {
+    paddingTop: Constants.statusBarHeight,
+    paddingBottom: 10,
+    backgroundColor: "#E9E9E9",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "stretch"
+  },
+
+
+  Body: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  NavBar: {
+    paddingBottom: Constants.statusBarHeight,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#E9E9E9",
+    alignSelf: "stretch",
+    paddingTop: 10,
+  },
+});
